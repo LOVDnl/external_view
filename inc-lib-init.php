de0869b34a80b111391a7094c6d14417d8ca0179
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2017-05-10 19:28:49 +0200

    Added basic functions (lovd_cleanDirName() and lovd_getInstallURL()).

---
+++ inc-lib-init.php
@@ -0,0 +1,64 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2015-06-17
+ * Modified    : 2017-05-10
+ * For LOVD    : 3.0-19
+ *
+ * Copyright   : 2004-2017 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+function lovd_cleanDirName ($s)
+{
+    // Cleans a given path by resolving a relative path.
+    if (!is_string($s)) {
+        // No input.
+        return false;
+    }
+
+    // Clean up the pwd; remove '\' (some PHP versions under Windows seem to escape the slashes with backslashes???)
+    $s = stripslashes($s);
+    // Clean up the pwd; remove '//'
+    $s = preg_replace('/\/+/', '/', $s);
+    // Clean up the pwd; remove '/./'
+    $s = preg_replace('/\/\.\//', '/', $s);
+    // Clean up the pwd; remove '/dir/../'
+    $s = preg_replace('/\/[^\/]+\/\.\.\//', '/', $s);
+
+    if (preg_match('/\/(\.)?\.\//', $s)) {
+        // Still not clean... Pff...
+        $s = lovd_cleanDirName($s);
+    }
+
+    return $s;
+}
+
+
+
+
+
+function lovd_getInstallURL ($bFull = true)
+{
+    // Returns URL that can be used in URLs or redirects.
+    return (!$bFull? '' : PROTOCOL . $_SERVER['HTTP_HOST']) . lovd_cleanDirName(dirname($_SERVER['SCRIPT_NAME']) . '/' . ROOT_PATH);
+}
